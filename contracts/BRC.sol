3ffdf92edd209bbe6e88d31c40e738d4774d86e4
BRICSRESERVE <bricsreservecurrency@proton.me>
2024-08-07 21:30:52 +0100

    Update BRC.sol

--- contracts/BRC.sol
+++ contracts/BRC.sol
@@ -8,10 +8,15 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BRC is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("BRC", "BRC") Ownable(msg.sender) {}
+contract BRC is ERC20, ERC20Burnable, ERC20Permit, Ownable {
+    constructor()
+        ERC20("BRICS Reserve Currency", "BRC")
+        ERC20Permit("BRICS Reserve Currency")
+        Ownable(msg.sender)
+    {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
